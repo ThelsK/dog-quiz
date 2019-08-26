207dfd34f77711ad60ad54857f653522a6c32fed
ThelsK <github@thels.nl>
2019-08-26 16:18:55 +0200

    Set up routing and index files.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from "react"
 import { Provider } from "react-redux"
-import { BrowserRouter, Route } from 'react-router-dom'
-import store from './store'
-import './App.css'
+import { BrowserRouter, Route } from "react-router-dom"
+import store from "./store"
+import "./App.css"
+import FetchData from "./components/FetchData/"
+import HomePage from "./components/HomePage/"
+import BreedListPage from "./components/BreedListPage/"
+import BreedPicturesPage from "./components/BreedPicturesPage/"
+import GamePage from "./components/GamePage/"
 
 class App extends React.Component {
   render() {
@@ -11,6 +16,11 @@ class App extends React.Component {
         <Provider store={store}>
           <div className="App">
             Mirte and Olga and Thels' Dog Quiz
+            <FetchData />
+            <Route exact path="/" component={HomePage} />
+            <Route path="/breeds/" component={BreedListPage} />
+            <Route path="/breed/:breedname" component={BreedPicturesPage} />
+            <Route path="/game/" component={GamePage} />
           </div>
         </Provider >
       </BrowserRouter>
@@ -18,4 +28,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App
---
+++ src/components/FetchData/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class FetchData extends React.Component {
+  render() {
+    return (
+      <div>
+        FetchData
+      </div>
+    )
+  }
+}
+
+export default FetchData
