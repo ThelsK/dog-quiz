f919637e369b7d3c45f4e4c630a3dafcbce8627e
ThelsK <github@thels.nl>
2019-08-27 15:26:36 +0200

    Created homepage. Updated App.js and App.css as well.

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
-        HomePage
+        <h1>MOTh's Dog Quiz</h1>
+        <Link to="/breeds/" className="App-link">
+          List of Dog Breeds
+        </Link>
       </div>
     )
   }
