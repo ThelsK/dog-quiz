5ee030cbbd3e8a3faa7d5cfd0568fff2ed7766d3
loklock <lokoschenko@gmail.com>
2019-08-27 15:02:13 +0200

    Refactor breed-pictures

--- src/components/BreedPicturesPage/index.js
+++ src/components/BreedPicturesPage/index.js
@@ -1,13 +1,23 @@
 import React from "react"
+import { connect } from 'react-redux'
 
 class BreedPicturesPage extends React.Component {
   render() {
+    // const currentBreed = this.props.dogList.find(breed => breed.name === this.props.name)
+    const currentBreed = this.props.dogList[0]
     return (
-      <div>
-        BreedPicturesPage
+      <div className="breed-pictures-box">
+        { this.props.images !== null && <img className="breed-picture" key={currentBreed.name} alt="breed" src={currentBreed.pictures}/>
+        })}
       </div>
     )
   }
 }
 
-export default BreedPicturesPage
+const mapStateToProps = (state) => {
+  return {
+    dogList: state.dogList
+  }
+}
+
+export default connect(mapStateToProps)(BreedPicturesPage)
