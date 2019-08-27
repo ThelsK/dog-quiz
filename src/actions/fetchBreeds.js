e6a8deac2f48f45d0672ede6b875160756fe7faa
Mirtles <mirte.post@yahoo.co.uk>
2019-08-27 14:41:19 +0200

    Clean up duplicate code and comments

--- src/actions/fetchBreeds.js
+++ src/actions/fetchBreeds.js
@@ -23,27 +23,3 @@ export const fetchBreeds = () => dispatch =>
           dispatch(action)
         })
     })
-
-
-// export const fetchBreeds = () => dispatch =>
-//   request('https://dog.ceo/api/breeds/list/all')
-//     .then(response =>
-//       dispatch(filterBreeds(response.body.message))
-//     )
-
-// const filterBreeds = (dogs) => {
-//   const dogsList = Object.keys(dogs).reduce((dogsList, dog) => {
-//     if (!dogs[dog].length) { return dogsList.concat([dog]) }
-//     return dogsList
-//   }, [])
-
-//   Promise.all(dogsList.map(breedname => fetchPictures(breedname)))
-//     .then(dogsList => {
-//       const action = ({ type: SET_BREEDS, payload: dogsList })
-//       dispatch(action)
-//     })
-// }
-
-// const fetchPictures = breedname =>
-//   request('https://dog.ceo/api/breed/' + breedname + '/images')
-//     .then(response => ({ breedname, pictures: response.body.message }))
