65a6b1f9a038716cd85d5c2a71adcd044f38afc6
ThelsK <github@thels.nl>
2019-08-28 17:27:34 +0200

    Fixed some of the link targets

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -17,12 +17,12 @@ class HomePage extends React.Component {
         </Link>
         <p>Play a game where you are given a single picture and have to guess the correct name of the dog breed from three different answers.</p>
         <br />
-        <Link to="/breeds/" className="App-link">
+        <Link to="/game/breedname" className="App-link">
           What does the dog look like
         </Link>
         <p>Play a game where you are given the name of a dog breed and have to guess which picture portrays a dog of that particular breed.</p>
         <br />
-        <Link to="/breeds/" className="App-link">
+        <Link to="/game/mixed" className="App-link">
           A mixture of both the other two games
         </Link>
         <p>Play a game where both of the above are mixed and matched. Sometimes you have to guess the name with the picture, and sometimes you have to guess the picture with the name.</p>
