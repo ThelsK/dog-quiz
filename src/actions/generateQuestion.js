c048078d89b1a46e7cc3b2975ab313a741c8c418
ThelsK <github@thels.nl>
2019-08-28 12:12:11 +0200

    Working on stuff

--- src/actions/generateQuestion.js
+++ src/actions/generateQuestion.js
@@ -1,7 +1,45 @@
 export const SET_CURRENT_QUESTION = 'SET_CURRENT_QUESTION'
 
-export const generateQuestion = (breedsList = []) => dispatch => {
-  console.log("We are generating a question.")
+export const generateQuestion = (breedsList = [], questionType = "picture", recentQuestions = [], totalAnswers = 3) => dispatch => {
+
+  if (!breedsList.length) {
+    console.log("ERROR: generateQuestion expects a list of breeds (each with a breedname and an array of pictures) as its first argument.")
+    return
+  }
+
+  if (totalAnswers > breedsList.length) {
+    totalAnswers = breedsList.length
+  }
+
+  if (questionType === "mixed") {
+    if (Math.random() < 0.5) {
+      questionType = "picture"
+    } else {
+      questionType = "breedname"
+    }
+  }
+
+  console.log("Generating Questions")
+
+  const correctIndexes = generateCorrectIndexes(breedsList, recentQuestions)
+  const incorrectIndexes = generateIncorrectIndexes(breedsList, correctIndexes.breedIndex, totalAnswers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   const action = ({
     type: SET_CURRENT_QUESTION,
     payload: {
@@ -24,4 +62,28 @@ export const generateQuestion = (breedsList = []) => dispatch => {
     },
   })
   dispatch(action)
+}
+
+const generateCorrectIndexes = (breedsList) => {
+
+  const breedIndex = Math.floor(Math.random() * breedsList.length)
+  const picturesList = breedsList[breedIndex].pictures
+  const pictureIndex = Math.floor(Math.random() * picturesList.length)
+
+  return ({
+    breedIndex,
+    pictureIndex,
+  })
+}
+
+const generateIncorrectIndexes = (breedsList, correctIndex, totalAnswers) => {
+  const incorrectIndexes = []
+  while (incorrectIndexes.length < totalAnswers - 1) {
+    const randomIndex = Math.floor(Math.random() * breedsList.length)
+    if (randomIndex !== correctIndex && !incorrectIndexes.includes[randomIndex]) {
+      incorrectIndexes.push(randomIndex)
+    }
+  }
+  console.log("Correct Index:", correctIndex)
+  console.log("Incorrect Indexes:", incorrectIndexes)
 }
--- src/components/GamePage/GamePageContainer.js
+++ src/components/GamePage/GamePageContainer.js
@@ -5,14 +5,10 @@ import { generateQuestion } from "../../actions/generateQuestion.js"
 class GamePageContainer extends React.Component {
 
   componentDidMount() {
-    console.log("The Game Page Container did mount.")
-    console.log("PROPS:", this.props)
     this.props.generateQuestion(this.props.breedsList)
-    console.log("The Game Page Container is done mounting.")
   }
 
   render() {
-    console.log("We are rendering the Game Page Container.")
     return (
       <div>
         GamePage
