d69b9374f90c45caab6a1f9b59b6a17f8c95495d
Mirtles <mirte.post@yahoo.co.uk>
2019-08-28 13:48:49 +0200

    Fix answer list  key error

--- src/components/QAPicture/QAPictureAnswers.js
+++ src/components/QAPicture/QAPictureAnswers.js
@@ -5,7 +5,7 @@ export default class QAPictureAnswers extends React.Component {
   render() {
     return (<div>
       <h2>answers:</h2>
-      {this.props.answers.map(answer => <p>{answer.breedname}</p>)}
+      {this.props.answers.map((answer, id) => <p key={id}>{answer.breedname}</p>)}
     </div>
     )
   }
