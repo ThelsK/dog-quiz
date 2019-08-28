7661b34c69b19e1c606d96c103b5fc55c913b580
loklock <lokoschenko@gmail.com>
2019-08-28 11:04:36 +0200

    Add randomize method to display 10 random pictures on the page, small fixes

--- src/components/BreedPicturesPage/index.js
+++ src/components/BreedPicturesPage/index.js
@@ -4,18 +4,41 @@ import './BreedPicturesPage.css'
 
 class BreedPicturesPage extends React.Component {
 
-  render() {
-    if (this.props.dogsList) {
+  randomize(array) {
+    console.log('I am inside randomize')
+    const expectedOutput = 10
+    if (array.length < expectedOutput) return array
+    let indexes = []
+    while (indexes.length < 10) {
+      const randNum = Math.floor(Math.random() * Math.floor(array.length))
+      if (!indexes.indexOf(randNum) > -1) {
+        indexes.push(randNum);
+      }
+    }
+    const output = indexes.reduce((output, index) => {
+      output.push(array[index])
+      return output
+    }, [])
+    return output
+  }
+
+  renderPictures() {
+    console.log('I am inside renderPictures')
       const currentBreedName = this.props.match.params.breedname
       const currentBreed = this.props.dogsList.find(breed => breed.breedname === currentBreedName)
+      const breedTenPictures = this.randomize(currentBreed.pictures)
       return (
         <div className="breed-pictures-box">
-          {currentBreed.pictures.map(breed => <img className="breed-picture" key={breed} alt="breed" src={breed} />)}
+          {breedTenPictures.map(breed => <img className="breed-picture" key={breed} alt="breed" src={breed} />)}
         </div>
       )
-    } else {
-      return <p>Loading...</p>
+  }
+
+  render() {
+    if (this.props.dogsList) {
+      return this.renderPictures()
     }
+    return <div>Loading...</div>
   }
 }
 
