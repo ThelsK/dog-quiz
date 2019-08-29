d4f7d8d0730e4bdbe79ebcfc61ce47d2e5015ec9
ThelsK <github@thels.nl>
2019-08-29 11:38:59 +0200

    lifted Answer States to container. Included scores.

--- src/components/GamePage/GamePageContainer.js
+++ src/components/GamePage/GamePageContainer.js
@@ -1,25 +1,73 @@
 import React from "react"
 import { connect } from "react-redux"
 import { generateQuestion } from "../../actions/generateQuestion.js"
-import QAPictureContainer from '../QAPicture/QAPictureContainer'
-import Scorebar from "../Scorebar"
+import { addCorrect, addWrong, resetScore } from "../../actions/score.js"
+import QAPictureContainer from '../QAPicture'
 
 class GamePageContainer extends React.Component {
+  state = {
+    answerStates: []
+  }
 
   componentDidMount() {
+    console.log("GamePageContainer Did Mount")
+    this.props.resetScore()
+
+    // Reset Score
+
+    // Reset Active Breeds
+
+    // Add Active Breeds (Parameters: BreedsList, ???Amount???)
+
+    // Generate Question
+
     this.props.generateQuestion(this.props.breedsList)
   }
 
+  receivedAnswer = event => {
+    const answerClicked = parseInt(event.target.id)
+    console.log("GamePageContainer Received Answer:", answerClicked)
+    this.setAnswerStates(this.props.currentQuestion.answers, answerClicked)
+
+    if (this.props.currentQuestion.answers[answerClicked].isCorrect) {
+      this.props.addCorrect()
+    } else {
+      this.props.addWrong()
+    }
+  }
+
+  setAnswerStates = (answers, answerClicked) => {
+    console.log("AnswerClicked:", answerClicked)
+    this.setState({
+      answerStates: answers.map((answer, answerID) => {
+        let answerClass = ""
+        console.log("answerID:", answerID)
+        console.log("Match:", (answerClicked === answerID))
+
+        if (answerClicked === answerID) {
+          answerClass += "Choose"
+        } else {
+          answerClass += "Show"
+        }
+
+        if (answer.isCorrect) {
+          answerClass += "Correct"
+        } else {
+          answerClass += "Wrong"
+        }
+
+        return answerClass
+      })
+    })
+  }
+
   render() {
-    return (
-      <div>
-        <h1>
-          Game Page
-        </h1>
-        <Scorebar />
-        <QAPictureContainer currentQuestion={this.props.currentQuestion} />
-      </div>
-    )
+    switch (this.props.currentQuestion.type) {
+      case "picture":
+        return <QAPictureContainer currentQuestion={this.props.currentQuestion} answerStates={this.state.answerStates} receivedAnswer={this.receivedAnswer} />
+      default:
+        return "Loading Quiz..."
+    }
   }
 }
 
@@ -29,6 +77,11 @@ const mapStateToProps = state =>
     currentQuestion: state.currentQuestion
   })
 
-const mapDispatchToProps = { generateQuestion }
+const mapDispatchToProps = {
+  generateQuestion,
+  addCorrect,
+  addWrong,
+  resetScore,
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(GamePageContainer)
--- src/components/QAPicture/QAPictureAnswers.js
+++ src/components/QAPicture/QAPictureAnswers.js
@@ -1,31 +1,20 @@
 import React from "react"
 
 export default class QAPictureAnswers extends React.Component {
-  state = {
-    answered: false
-  }
-
-  checkCorrect = (event) => {
-    this.setState({
-      answered: true
-    })
-    // if (this.props.answers[event.target.id].isCorrect) {
-    //   console.log("correct!")
-    // } else {
-    //   console.log("false")
-    // }
-  }
 
   render() {
     return (
       <div>
-        <h2>answers:</h2>
+        <h2>Answers:</h2>
         <div className="answers">
           {this.props.answers.map((answer, id) =>
             <div
-              className={"answer " + (!this.state.answered ? null : (answer.isCorrect ? "correct" : "wrong"))}
-              key={id} id={id}
-              onClick={this.checkCorrect}
+              className={(this.props.answerStates.length > id)
+                ? "answer answer".concat(this.props.answerStates[id])
+                : "answer answerWaiting"}
+              key={id}
+              id={id}
+              onClick={this.props.receivedAnswer}
             >
               {answer.breedname}
             </div>
