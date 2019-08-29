adce51826a022f9b03fb459eff52be1921764153
loklock <lokoschenko@gmail.com>
2019-08-29 16:05:31 +0200

    Add style to game page

--- src/components/Scorebar/index.js
+++ src/components/Scorebar/index.js
@@ -15,9 +15,14 @@ class Scorebar extends React.Component {
     const correctScorePercent = Math.round((correctScore * 100) / totalScore) || 0
     const streak = this.props.streak
     return (<div className="scorebar">
-      <p>Total Score: {correctScore}/{totalScore}</p>
-      <p className="scorebar-correct">{correctScorePercent}%</p>
-      <p>Current Streak: {streak}</p>
+      <div className="scorebar-left">
+        <p className="scorebar-percent">{correctScorePercent}%</p>
+        <p className="scorebar-correct">correct</p>
+        <p className="scorebar-total-score">({correctScore} of {totalScore})</p>
+      </div>
+      <div className="scorebar-right">
+        <p className="scorebar-current-streak">Current Streak: {streak}</p>
+      </div>
     </div>)
   }
 }
