dda27e6cd24416d112a1aa18bc5aab75d6a9acf3
loklock <lokoschenko@gmail.com>
2019-08-29 15:11:17 +0200

    Style game page

--- src/components/QAPicture/QAPictureQuestion.js
+++ src/components/QAPicture/QAPictureQuestion.js
@@ -3,8 +3,8 @@ import React from "react"
 export default class QAPictureQuestion extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Identify the dog in the picture</h2>
+      <div className="q-box">
+        <h5>Identify the dog in the picture</h5>
         <img className="question" src={this.props.question} alt={"guess what dog this is"} />
       </div>
     )
